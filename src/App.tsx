c66b1ca8b1e42fad627b3385ad95047ded15f25a
JD Rowley <jdrowley1989@gmail.com>
2022-01-31 19:24:44 -0700

    added functionality to add todos button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ToDoList from './components/ToDoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
+import { v4 as uuidv4 } from 'uuid';
 
 const App: React.FC = () => {
-  const todos = [{id: 't1', text: 'Finish the course'}];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos => [
+      ...prevTodos,
+      { id: uuidv4(), text: text }
+    ]);
+  };
 
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <ToDoList items={todos} />
     </div>
   );
